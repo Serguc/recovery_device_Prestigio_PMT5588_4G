eb6e81b23e60245f5bbae511205a1955977657a0
serguc <42298249+Serguc@users.noreply.github.com>
2025-10-11 12:06:55 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \ 
-	$(LOCAL_DIR)/omni_PMT5588_4G.mk
+	$(LOCAL_DIR)/omni_CF80A5L.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_CF80A5L-user \
+    omni_CF80A5L-userdebug \
+    omni_CF80A5L-eng
