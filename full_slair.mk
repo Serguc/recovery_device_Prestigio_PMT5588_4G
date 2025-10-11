97f598b699ec8b7383d836bd9c4532d54bc45d73
serguc <42298249+Serguc@users.noreply.github.com>
2025-10-11 11:04:48 +0300

    Add files via upload

---
+++ full_slair.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+PRODUCT_DEVICE := slair
+PRODUCT_NAME := omni_slair
+PRODUCT_BRAND := Prestigio
+PRODUCT_MODEL := slair
+PRODUCT_MANUFACTURER := Prestigio
+PRODUCT_BOARD := mt6735
---
+++ omni_slair.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, device/Prestigio/CF80A5L/full_slair.mk)
+
+PRODUCT_NAME := omni_slair
