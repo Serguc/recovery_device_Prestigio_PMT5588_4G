cda1d2568780d620f26ee49c6ffc2a83ff864305
serguc <42298249+Serguc@users.noreply.github.com>
2025-10-11 21:52:27 +0300

    Update and rename omni_CF80A5L.mk to omni_PMT5588_4G.mk

--- omni_CF80A5L.mk
+++
@@ -1,5 +0,0 @@
-$(call inherit-product, vendor/omni/config/common.mk)
-
-$(call inherit-product, device/Prestigio/PMT5588_4G/full_CF80A5L.mk)
-
-PRODUCT_NAME := omni_CF80A5L
---
+++ omni_PMT5588_4G.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, device/Prestigio/PMT5588_4G/full_PMT5588_4G.mk)
+
+PRODUCT_NAME := omni_PMT5588_4G
